c7e054054679f00d1c77268c5d82f8cb84a1a7f3
Fabian Bleile <fabian.bleile@posteo.de>
2026-02-22 22:36:43 +0100

    no message

--- src/experiment/jobfarm.sh
+++ src/experiment/jobfarm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH -D /dss/dsshome1/0C/ge86xim2/steinstadion
+#SBATCH -D /dss/dsshome1/0C/ge86xim2/midagma
 #SBATCH -o slurm_logs/jobfarm.%N.%j.out
 #SBATCH -J JobFarm
-#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=fabian.bleile@posteo.de
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --get-user-env
+#SBATCH --export=ALL
+
 #SBATCH --clusters=cm4
 #SBATCH --partition=cm4_std
 #SBATCH --qos=cm4_std
@@ -12,22 +14,34 @@
 #SBATCH --ntasks=200
 #SBATCH --cpus-per-task=2
 #SBATCH --time=24:00:00
-#SBATCH --mail-type=NONE
-#SBATCH --export=ALL
+
 module load slurm_setup
 module load jobfarm
+
 source /dss/dsshome1/0C/ge86xim2/miniconda3/etc/profile.d/conda.sh
-conda activate steinstadion-env
+conda activate strucmi
+
 export TMPDIR=/tmp
 export MPLCONFIGDIR=/tmp/matplotlib
 mkdir -p /tmp/matplotlib
-export PYTHONPATH=$PYTHONPATH:/dss/dsshome1/0C/ge86xim2/steinstadion
+
+# Make repo imports work (src.*)
+export PYTHONPATH="/dss/dsshome1/0C/ge86xim2/midagma:$PYTHONPATH"
+
 # Use the existing command list
 taskdb="src/experiment/cluster_command_list"
 txt_file="${taskdb}.txt"
 echo "$txt_file"
-# delete prev jobfarm db
+
+# Ensure log dir + command list file exist
+mkdir -p slurm_logs
+mkdir -p "$(dirname "$txt_file")"
+touch "$txt_file"
+
+# Delete previous jobfarm DB/results
 rm -f "${taskdb}.db"
 rm -rf "${taskdb}.txt_res"
+
 # Start JobFarm
-jobfarm start $txt_file
+jobfarm start "$txt_file"
+
