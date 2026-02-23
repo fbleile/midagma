0fa3a67c105f07c658c7dc9eaf465f91d51e7719
Fabian Bleile <fabian.bleile@posteo.de>
2026-02-23 14:04:55 +0100

    no message

--- src/experiment/experiment_manager.py
+++ src/experiment/experiment_manager.py
@@ -363,7 +363,6 @@ class ExperimentManager:
         
             cmd_args = dict(
                 command_list=command_list,
-                array_indices=range(1, len(command_list)+1),
                 mode=self.compute,
                 dry=self.dry,
                 prompt=False,
